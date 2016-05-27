027bfc32f8abdd385c1727a10416575883d0de11
Ryan White <ryan.white@fireeye.com>
2016-05-27 17:50:59 -0400

    Add macros for long functions that screw up keymap formatting

--- keyboard/kc60/keymaps/keymap_default.c
+++ keyboard/kc60/keymaps/keymap_default.c
@@ -20,6 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSE   2 // mouse layer
 #define LED     3 // LED control
 
+#define F_SPACEFN LT(SPACEFN, KC_SPACE)
+#define F_LCTLESC MT(MOD_LCTL, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layout 0: Default Layer
      * ,-----------------------------------------------------------.
@@ -35,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [BASE] = KEYMAP(
-        F(0),                 KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, \
-        KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
-        MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
+        F(0),      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, \
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
+        F_LCTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
 
-        KC_LSPO, KC_Z,        KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSPC, \
-        KC_LCTL, KC_LGUI,     KC_LALT,             LT(SPACEFN, KC_SPACE),                             KC_RALT, MO(LED), KC_APP,  KC_RCTL),
+        KC_LSPO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSPC, \
+        KC_LCTL,   KC_LGUI, KC_LALT,             F_SPACEFN,                       KC_RALT, MO(LED), KC_APP,  KC_RCTL),
  
     [SPACEFN] = KEYMAP(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
