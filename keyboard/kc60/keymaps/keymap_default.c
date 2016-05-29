bd53f045c0d5f6c967d32b5d527e7e212e0697d3
Ryan White <ryan.white@fireeye.com>
2016-05-28 20:40:46 -0400

    Rename macros and layers, add game layer

--- keyboard/kc60/keymaps/keymap_default.c
+++ keyboard/kc60/keymaps/keymap_default.c
@@ -15,13 +15,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "kc60.h"
 #include "keymap_common.h"
 
-#define BASE    0 // default layer
-#define SPACEFN 1 // space fn layer
-#define MOUSE   2 // mouse layer
-#define LED     3 // LED control
+#define _BASE    0 // default layer
+#define _GAME    1 // Simplified layout for gaming
+#define _FUNC    2 // space fn layer
+#define _UTIL    3 // LED control
 
-#define F_SPACEFN LT(SPACEFN, KC_SPACE)
-#define F_LCTLESC MT(MOD_LCTL, KC_ESC)
+#define _SPACEFN LT(_FUNC, KC_SPACE)
+#define _LCTLESC MT(MOD_LCTL, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layout 0: Default Layer
@@ -37,28 +37,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Fn1 |Gui |Alt |        SpaceFn         |Alt |Gui |App |Ctrl|
      * `-----------------------------------------------------------'
      */
-    [BASE] = KEYMAP(
-        F(0),      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, \
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
-        F_LCTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
+    [_BASE] = KEYMAP(
+        F(0),     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_BSPC, \
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS, \
+        _LCTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,  \
 
-        KC_LSPO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSPC, \
-        KC_LCTL,   KC_LGUI, KC_LALT,             F_SPACEFN,                       KC_RALT, MO(LED), KC_APP,  KC_RCTL),
+        KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                      KC_RSPC, \
+        KC_LCTL,  KC_LALT, KC_LGUI,          _SPACEFN,                                    KC_RALT, KC_RGUI, OSL(_UTIL), KC_RCTL),
+
+    [_GAME] = KEYMAP(
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, \
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,       KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_TRNS, \
+        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,       KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
+
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH,                    KC_UP, \
+        KC_LCTL,   KC_LALT, MO(_FUNC),        KC_SPC,                             OSL(_UTIL), KC_LEFT, KC_DOWN, KC_RGHT),
  
-    [SPACEFN] = KEYMAP(
+    [_FUNC] = KEYMAP(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-        KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_SLCK, KC_PAUS, KC_TRNS,  \
+        KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_SLCK, KC_PAUS, KC_BSLS, \
         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS,       KC_PENT, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,                  KC_TRNS, \
-        KC_CAPS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT),
+        KC_CAPS, KC_TRNS, KC_TRNS,          KC_TRNS,                            KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT),
 
-    [LED] = KEYMAP(
-        KC_NO,  F(1),   F(2),   F(3),   F(4),   F(5),   F(6),   F(7),   F(8),   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
-        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  BL_DEC, BL_INC, BL_TOGG, \
-        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO, \
-        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO, \
-        KC_NO,  KC_NO,  KC_NO,         KC_TRNS,                                 KC_NO,  KC_TRNS, KC_NO, KC_NO),
-
+    [_UTIL] = KEYMAP(
+        KC_NO,  F(1),   F(2),   F(3),   F(4),   F(5),   F(6),   F(7),   F(8),   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO, \
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   BL_DEC, BL_INC, TG(_GAME), \
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,          KC_NO, \
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,          KC_NO, \
+        KC_NO,  KC_NO,  KC_NO,          KC_TRNS,                                KC_TRNS, KC_NO,   KC_TRNS, KC_NO),
 
 };
 enum function_id {
